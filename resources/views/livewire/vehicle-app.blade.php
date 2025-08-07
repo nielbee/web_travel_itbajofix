8af7e702440b1ac2a5d7b84826440deb95012533
DanylBeeh <dany151011@gmail.com>
2025-08-07 13:34:02 +0800

    sewa motor dan paket trip su jadi, api su jadi, tambahan pembayaran pakai qris, klo perlu

--- resources/views/livewire/vehicle-app.blade.php
+++ resources/views/livewire/vehicle-app.blade.php
@@ -37,6 +37,7 @@
                 <td class="px-4 py-2">{{$v->model}}</td>
                 <td class="px-4 py-2">Rp. {{$v->price}}</td>
                 <td class="px-4 py-2">
+            
                     @if ($v->availability)
                         <span class="text-green-600">Available</span>
                     @else
@@ -49,10 +50,13 @@
                     >
                         Toggle Availability
                     </a>
-                    <a href='vehicles/delete/{{ $v->plate_number }}' onclick= "return confirmLink()"     
-                       class ="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600 transition"
-                    >  
-                        Delete
+                </td>
+                <td class="px-4 py-2">
+                     <a href='vehicles/delete/{{ $v->plate_number }}' onclick= "return confirmLink()"     
+                       >  
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-red-600 hover:text-red-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6M1 7h22M9 7V4a1 1 0 011-1h4a1 1 0 011 1v3" />
+                     </svg>
                     </a>
                 </td>
             </tr>
